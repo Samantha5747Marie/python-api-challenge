cf75683b9f5d0d64f0115e5465213300c837805e
Samantha5747Marie <Samanthamarie5747@gmail.com>
2023-11-16 17:57:24 -0600

    final homework files

--- Starter_Code (8)/Starter_Code/starter_code/api_keys.py
+++ Starter_Code (8)/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "415f904032c5e472969e4819182a0fff"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "37427798e9f14bbaa1c6fd2bb43d4e1b"
